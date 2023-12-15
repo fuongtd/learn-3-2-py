fa9a189117b756facf791c41bf2b3dd2b601b7a2
fuongtd <fuongtd@users.noreply.github.com>
2023-12-15 10:28:13 +0700

    Create 32.py

---
+++ 32.py
@@ -0,0 +1,8 @@
+import datetime
+
+current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+text_to_write = f"Updated on {current_time}."
+
+with open("README.md", "w") as file:
+    file.write(text_to_write)
