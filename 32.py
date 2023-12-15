0889b4f51d8c2dc87e4a307f67392ce34f607fef
fuongtd <fuongtd@users.noreply.github.com>
2023-12-15 12:15:16 +0700

    Update 32.py

--- 32.py
+++ 32.py
@@ -4,5 +4,5 @@ current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 text_to_write = f"Updated on {current_time}."
 
-with open("system.py", "w") as file:
-    file.write(text_to_write)
+with open("system.py", "a") as file:
+    file.write(text_to_write + "\n")
