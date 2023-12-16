4e0a12a6a01a2022eedf72b36c5250ac7644d142
fuongtd <fuongtd@users.noreply.github.com>
2023-12-16 14:33:21 +0700

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,14 +1,8 @@
 import datetime
-import random
-import string
 
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 text_to_write = f"Updated on {current_time}."
 
-random_chars = ''.join(random.choices(string.ascii_letters, k=2))
-random_nums = ''.join(random.choices(string.digits, k=4))
-file_name = f"{random_chars}{random_nums}.py"
-
-with open(file_name, "a") as file:
+with open("system.py", "a") as file:
     file.write(text_to_write + "\n")
