29e4adc8f84c310086726df58aa552de77829243
fuongtd <fuongtd@users.noreply.github.com>
2023-12-16 14:24:54 +0700

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,22 +1,19 @@
-import os
+import datetime
 import random
 import string
-import datetime
 
-random_chars = ''.join(random.choices(string.ascii_lowercase, k=2))
-random_numbers = ''.join(random.choices(string.digits, k=4))
+# Tạo chuỗi ngẫu nhiên xx
+random_chars = ''.join(random.choices(string.ascii_letters, k=2))
+
+# Tạo số ngẫu nhiên yyyy
+random_numbers = random.randint(1000, 9999)
+
+# Tạo tên tệp tin xxyyyy.py
 file_name = f"{random_chars}{random_numbers}.py"
 
-code_directory = "./code"
-
-if not os.path.exists(code_directory):
-    os.makedirs(code_directory)
-
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
 text_to_write = f"Updated on {current_time}."
 
-file_path = os.path.join(code_directory, file_name)
-
-with open(file_path, "a") as file:
+# Mở tệp tin mới và ghi nội dung vào tệp
+with open(file_name, "a") as file:
     file.write(text_to_write + "\n")
