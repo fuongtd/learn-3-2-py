68b98baac1d9121a6a047c3b29d0093cf782896f
fuongtd <fuongtd@users.noreply.github.com>
2023-12-16 14:27:00 +0700

    Update 32.py

--- 32.py
+++ 32.py
@@ -2,18 +2,13 @@ import datetime
 import random
 import string
 
-# Tạo chuỗi ngẫu nhiên xx
-random_chars = ''.join(random.choices(string.ascii_letters, k=2))
-
-# Tạo số ngẫu nhiên yyyy
-random_numbers = random.randint(1000, 9999)
-
-# Tạo tên tệp tin xxyyyy.py
-file_name = f"{random_chars}{random_numbers}.py"
-
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
 text_to_write = f"Updated on {current_time}."
 
-# Mở tệp tin mới và ghi nội dung vào tệp
+random_chars = ''.join(random.choices(string.ascii_letters, k=2))
+random_nums = ''.join(random.choices(string.digits, k=4))
+file_name = f"{random_chars}{random_nums}.py"
+
 with open(file_name, "a") as file:
     file.write(text_to_write + "\n")
