c43849ca5ae1da74463b11f0222164464b127096
fuongtd <fuongtd@users.noreply.github.com>
2023-12-16 14:20:34 +0700

    Update 32.py

--- 32.py
+++ 32.py
@@ -1,8 +1,22 @@
+import os
+import random
+import string
 import datetime
+
+random_chars = ''.join(random.choices(string.ascii_lowercase, k=2))
+random_numbers = ''.join(random.choices(string.digits, k=4))
+file_name = f"{random_chars}{random_numbers}.py"
+
+code_directory = "./code"
+
+if not os.path.exists(code_directory):
+    os.makedirs(code_directory)
 
 current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 text_to_write = f"Updated on {current_time}."
 
-with open("system.py", "a") as file:
+file_path = os.path.join(code_directory, file_name)
+
+with open(file_path, "a") as file:
     file.write(text_to_write + "\n")
